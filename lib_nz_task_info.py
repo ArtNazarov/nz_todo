20693caaf2bf1651de49e25dcdd6bb3d6c0f2ed2
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-12-11 21:49:42 +0500

    Update lib_nz_task_info.py

--- lib_nz_task_info.py
+++ lib_nz_task_info.py
@@ -1,14 +1,26 @@
-import os
-from lib_nz_tasks import *
-from lib_nz_current_path import *
+import lib_nz_tasks
+import lib_nz_current_path
 
 
 def get_task_info_path_by_id(project_id: str, task_id: str) -> str:
-    current_path = get_cur_path()
-    project_path = os.path.join(current_path, f"project_{project_id}")
-    tasks_path = os.path.join(project_path, f"task_{task_id}")
-    if not os.path.exists(tasks_path):
-        os.makedirs(tasks_path)
+    """
+    Возвращает абсолютный путь к файлу
+    index.tasks для заданной задачи в проекте
+
+    Args:
+        project_id (str): ID проекта
+        task_id (str):ID задачи
+
+    Returns:
+        str: Путь к файлу вида /path/to/index.tasks
+
+    """
+    project_path = lib_nz_current_path.os.path.join(
+        lib_nz_current_path.current_path(), f"project_{project_id}")
+    tasks_path = lib_nz_current_path.os.path.join(
+        project_path, f"task_{task_id}")
+    if not lib_nz_current_path.os.path.exists(tasks_path):
+        lib_nz_current_path.os.makedirs(tasks_path)
     return tasks_path
 
 
@@ -18,21 +30,22 @@ def save_task_info(project_id: str, task_info: dict) -> None:
 
     Параметры:
     project_id (str): Уникальный строковый ID проекта
-    task_info (dict): Словарь ключ-значение, где ключи - атрибуты заметки (caption, description, priority).
+    task_info (dict): Словарь ключ-значение, где ключи - атрибуты заметки
+    (caption, description, priority).
     """
     # Создаем каталог согласно ключа
     task_id = task_info['task_id']
-    current_path = get_cur_path()
-    add_task_id(project_id, task_id)
+    lib_nz_tasks.add_task_id(project_id, task_id)
     task_info_path = get_task_info_path_by_id(project_id, task_id)
     # print(task_info_path);
 
     # Сохраним атрибуты в отдельные файлы
     for key, value in task_info.items():
         # получаем полный путь к файлу
-        file_path = os.path.join(task_info_path, f"{task_id}.{key}")
+        file_path = lib_nz_current_path.os.path.join(
+            task_info_path, f"{task_id}.{key}")
         # открываем на запись
-        with open(file_path, 'w') as file:
+        with open(file_path, 'w', encoding='utf-8') as file:
             # пишем строковое значение
             file.write(value)
 
@@ -49,18 +62,17 @@ def read_task_info(project_id: str, task_id: str) -> dict[str, str]:
     dict: Словарь ключ-значение, информация по ключам извлекается из файлов
     """
     task_info = {}
-    current_path = get_cur_path()
     task_path = get_task_info_path_by_id(project_id, task_id)
 
     # Проверим, что путь к проекту существует
-    if os.path.exists(task_path):
+    if lib_nz_current_path.os.path.exists(task_path):
         # Считываем атрибуты по файлам
-        for filename in os.listdir(task_path):
+        for filename in lib_nz_current_path.os.listdir(task_path):
             key = filename.split('.')[1]  # Извлечем атрибут
             # получаем имя файла
-            file_path = os.path.join(task_path, filename)
+            file_path = lib_nz_current_path.os.path.join(task_path, filename)
             # откроем на считывание
-            with open(file_path, 'r') as file:
+            with open(file_path, 'r', encoding='utf-8') as file:
                 # забираем значение атрибута
                 # очищаем от строку слева и справа
                 task_info[key] = file.read().strip()
@@ -70,11 +82,11 @@ def read_task_info(project_id: str, task_id: str) -> dict[str, str]:
 
 def is_attributes_of_task_exists(project_id: str, task_id: str) -> bool:
     """
-    Существует ли каталог с проектом    
+    Существует ли каталог с проектом
     """
     task_path = get_task_info_path_by_id(project_id, task_id)
     # Проверим, что путь к проекту существует
-    return os.path.exists(task_path)
+    return lib_nz_current_path.os.path.exists(task_path)
 
 
 def get_attributes_of_task(project_id: str, task_id: str) -> set[str]:
@@ -95,7 +107,7 @@ def get_attributes_of_task(project_id: str, task_id: str) -> set[str]:
     # Проверим, что путь к проекту существует
     if is_attributes_of_task_exists(project_id, task_id):
         # Считываем атрибуты по файлам
-        for filename in os.listdir(task_path):
+        for filename in lib_nz_current_path.os.listdir(task_path):
             key = filename.split('.')[1]  # Извлечем атрибут
             attrs |= {key}
 
