5d5710750d23e994d6333a4b88d64973c9b7f1f1
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-12-11 19:05:01 +0500

    Added types

--- lib_nz_commandline.py
+++ lib_nz_commandline.py
@@ -1,7 +1,8 @@
 import sys
 import re
 
-def get_mode_from_commandline():
+
+def get_mode_from_commandline() -> str:
     """
     Получает режим операции
 
@@ -13,7 +14,7 @@ def get_mode_from_commandline():
     return "dialog"
 
 
-def get_operation_from_commandline():
+def get_operation_from_commandline() -> str:
     """
     Получает код операции
 
@@ -24,7 +25,8 @@ def get_operation_from_commandline():
             return sys.argv[i].split('=')[1]
     return ""
 
-def get_project_id_from_commandline():
+
+def get_project_id_from_commandline() -> str:
     """
     Ищет id проекта в аргументах
 
@@ -35,7 +37,8 @@ def get_project_id_from_commandline():
             return sys.argv[i].split('=')[1]
     return ""
 
-def get_task_id_from_commandline():
+
+def get_task_id_from_commandline() -> str:
     """
     Ищет id задачи в аргументах
 
@@ -47,7 +50,7 @@ def get_task_id_from_commandline():
     return ""
 
 
-def get_record_from_commandline(project_id, attributes):
+def get_record_from_commandline(project_id: str, attributes: tuple[str, ...] | list[str]) -> dict[str, str]:
     """
     Составляет словарь по командной строке
 
@@ -56,14 +59,14 @@ def get_record_from_commandline(project_id, attributes):
     record = dict()
     n = len(sys.argv)
     for attr in attributes:
-        record[attr] = "" # начальное значение
+        record[attr] = ""  # начальное значение
         for i in range(1, n):
             if f".{attr}=" in sys.argv[i]:
                 record[attr] = sys.argv[i].split('=')[1]
     return record
 
 
-def get_record_about_task_from_commandline(project_id, task_id, attributes_of_task):
+def get_record_about_task_from_commandline(project_id: str, task_id: str, attributes_of_task: tuple[str, ...] | list[str]) -> dict[str, str]:
     """
     Составляет словарь по командной строке
 
@@ -72,14 +75,14 @@ def get_record_about_task_from_commandline(project_id, task_id, attributes_of_ta
     record = dict()
     n = len(sys.argv)
     for attr in attributes_of_task:
-        record[attr] = "" # начальное значение
+        record[attr] = ""  # начальное значение
         for i in range(1, n):
             if f".{attr}=" in sys.argv[i]:
                 record[attr] = sys.argv[i].split('=')[1]
     return record
 
 
-def get_attributes_from_commandline():
+def get_attributes_from_commandline() -> set[str]:
     """
     Составляет множество атрибутов по командной строке
 
@@ -87,7 +90,7 @@ def get_attributes_from_commandline():
     """
     attrs = set()
     n = len(sys.argv)
-   
+
     # Регулярное выражение для извлечения значений
     pattern = r"\.(\w+)="
 
@@ -102,5 +105,3 @@ def get_attributes_from_commandline():
             # Добавляем найденное значение в список результатов
             results.append(match.group(1))
     return results
-
-
